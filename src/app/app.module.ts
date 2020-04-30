e378a20d8ed48dc3ccdde5cf60d7d248cf832bc6
Sarah <sarahamann96@gmail.com>
2020-04-30 10:27:57 -0500

    added fade animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './modules/home/home.module';
@@ -12,6 +12,7 @@ import { PortfolioModule } from './modules/portfolio/portfolio.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HomeModule,
     PortfolioModule
